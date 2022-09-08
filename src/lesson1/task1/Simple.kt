d2e1b11d0e2e6b52edf89bd1acb8a6f80e3d50ef
ArsZy <wotar.stan@yandex.ru>
2022-09-08 14:38:54 +0300

    lesson1_1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,8 +74,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):
-        Double = ((sagenes * 48 + arshins * 16 + vershoks) * 4.445) / 100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    ((sagenes * 48 + arshins * 16 + vershoks) * 4.445) / 100
 
 /**
  * Тривиальная (1 балл)
@@ -83,7 +83,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (PI * deg) / 180 + (PI * min) / 10800 + (PI * sec) / 648000
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
+    (PI * deg) / 180 + (PI * min) / 10800 + (PI * sec) / 648000
 /**
  * Тривиальная (1 балл)
  *
