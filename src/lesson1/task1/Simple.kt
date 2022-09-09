3d56095dd0b37256f6bf43d9332bb4461f237fd1
ArsZy <wotar.stan@yandex.ru>
2022-09-09 12:10:25 +0300

    lesson1_2

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,8 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minutes * 60 + seconds
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int =
+    hours * 3600 + minutes * 60 + seconds
 
 /**
  * Тривиальная (1 балл)
@@ -91,8 +92,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double):
-        Double = sqrt(abs(x2 - x1).pow(2) + abs(y2 - y1).pow(2))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+    sqrt(abs(x2 - x1).pow(2) + abs(y2 - y1).pow(2))
 
 /**
  * Простая (2 балла)
@@ -119,7 +120,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = ((100 + percent) / 100.0).pow(3) * initial
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+    ((100 + percent) / 100.0).pow(3) * initial
 
 /**
  * Простая (2 балла)
@@ -127,4 +129,5 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = ((100 + percent) /
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number % 10) * 100 + ((number / 10) % 10) * 10 + number / 100
+fun numberRevert(number: Int): Int =
+    (number % 10) * 100 + ((number / 10) % 10) * 10 + number / 100
