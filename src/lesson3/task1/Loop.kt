fa87376eb7670a33e43a97033b413d40c674c049
ArsZy <wotar.stan@yandex.ru>
2022-10-12 09:04:59 +0300

    lesson3_6

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -170,7 +170,7 @@ fun lcm(m: Int, n: Int): Int = m * n / multiple(m, n)
 fun isCoPrime(m: Int, n: Int): Boolean {
     if (m == 1 || n == 1) return true
     if (maxOf(m, n) % minOf(m, n) == 0) return false
-    for (i in 2..sqrt(maxOf(m, n).toDouble()).toInt()) {
+    for (i in 2..sqrt(minOf(m, n).toDouble()).toInt() step 2) {
         if (!isPrime(i)) continue
         if ((m % i == 0) && (n % i == 0)) return false
     }
