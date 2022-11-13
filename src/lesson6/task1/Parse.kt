1f6b3017201e0b2c105203ea44a136b3b3b5fab7
ArsZy <wotar.stan@yandex.ru>
2022-11-13 23:31:06 +0300

    lesson6_3_1

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -133,7 +133,11 @@ fun dateDigitToStr(digital: String): String {
  *
  * PS: Дополнительные примеры работы функции можно посмотреть в соответствующих тестах.
  */
-fun flattenPhoneNumber(phone: String): String = TODO()
+fun flattenPhoneNumber(phone: String): String {
+    val p1 = phone.split(" ")
+    if (p1.size != 3) return ""
+    return TODO()
+}
 
 /**
  * Средняя (5 баллов)
